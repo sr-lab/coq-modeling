a2a674de504e4105095d6811079858085e4d8328
Martim Afonso <martimafonso185@gmail.com>
2025-05-09 17:21:20 +0100

    refactoring reward_goals

--- src/tactic_gen/reward_utils.py
+++ src/tactic_gen/reward_utils.py
@@ -6,8 +6,12 @@ def reward_goals(initial_goals, final_goals):
     Compare the initial goals with the final goals.
     """
     if (
-        (initial_goals is None and final_goals is None)
-        or (initial_goals.goals is None and final_goals.goals is None)
+        ((initial_goals is None or final_goals is None) 
+        and initial_goals == final_goals)
+        or ((initial_goals.goals is None and final_goals.goals is None) 
+        and initial_goals.goals == final_goals.goals)
+        or ((initial_goals.goals.goals is None and final_goals.goals.goals is None)
+        and initial_goals.goals.goals == final_goals.goals.goals)
     ):
         return 1
     elif final_goals is None or final_goals.goals is None:
