a8f25d89e76a409ab5df36f6d47ae5d59288684b
Martim Afonso <martimafonso185@gmail.com>
2025-05-09 17:21:51 +0100

    refactoring reward_goals

--- src/tactic_gen/reward_utils.py
+++ src/tactic_gen/reward_utils.py
@@ -8,9 +8,9 @@ def reward_goals(initial_goals, final_goals):
     if (
         ((initial_goals is None or final_goals is None) 
         and initial_goals == final_goals)
-        or ((initial_goals.goals is None and final_goals.goals is None) 
+        or ((initial_goals.goals is None or final_goals.goals is None) 
         and initial_goals.goals == final_goals.goals)
-        or ((initial_goals.goals.goals is None and final_goals.goals.goals is None)
+        or ((initial_goals.goals.goals is None or final_goals.goals.goals is None)
         and initial_goals.goals.goals == final_goals.goals.goals)
     ):
         return 1
