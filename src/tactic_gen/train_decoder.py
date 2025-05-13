2c2f76680a94ecbe57a6c772c28c37cc6ef8eff4
Martim Afonso <martimafonso185@gmail.com>
2025-05-13 18:06:16 +0100

    remove bitsandbytes

--- src/tactic_gen/train_decoder.py
+++ src/tactic_gen/train_decoder.py
@@ -135,17 +135,17 @@ def get_lora_conf(conf: dict[str, Any]) -> LoraConfig:
 
 
 def get_model(model_name: str) -> PreTrainedModel:
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_storage=torch.bfloat16,
-    )
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.bfloat16,
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_quant_storage=torch.bfloat16,
+    # )
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        quantization_config=bnb_config,
+        #quantization_config=bnb_config,
         torch_dtype=torch.bfloat16,
         #device_map="auto"
     )
