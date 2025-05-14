b150ee1a33d68ef0a0c791d7f37777dbd2a69875
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2025-05-14 21:01:59 +0100

    small fix

--- src/tactic_gen/reward_utils.py
+++ src/tactic_gen/reward_utils.py
@@ -32,7 +32,7 @@ def reward_goals(ground_truth_goals, final_goals):
     else:
         goals_reward_ty = 0
         goals_reward_hyps = 0
-        for i in enumerate(ground_truth_goals.goals.goals):
+        for i in range(len(ground_truth_goals.goals.goals)):
             embedding1_ty = model.encode(ground_truth_goals.goals.goals[i].ty, convert_to_tensor=True)
             embedding2_ty = model.encode(final_goals.goals.goals[i].ty, convert_to_tensor=True)
             similarity_ty = util.cos_sim(embedding1_ty, embedding2_ty).item()
