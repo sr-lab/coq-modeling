c5c0670ec3d08a7755e520d628c7b7964807c42a
Martim Afonso <martimafonso185@gmail.com>
2025-05-14 18:05:46 +0100

    changed accelerator config

--- src/util/train_utils.py
+++ src/util/train_utils.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 from pathlib import Path
 from enum import Enum
+import yaml
 
 from yaml import load, Loader
 from transformers import TrainingArguments, PreTrainedTokenizer
@@ -149,6 +150,8 @@ def get_training_args(
     )
 
 def get_grpo_training_args(conf: dict[str, Any], local_rank: Optional[int]) -> GRPOConfig:
+    with open("/root/.cache/huggingface/accelerate/default_config.yaml", "r") as f:
+        conf = yaml.safe_load(f)
     return GRPOConfig(
         output_dir=get_required_arg("output_dir", conf),
         per_device_train_batch_size=get_required_arg(
@@ -173,6 +176,6 @@ def get_grpo_training_args(conf: dict[str, Any], local_rank: Optional[int]) -> G
         local_rank=(local_rank if local_rank else -1),
         ddp_find_unused_parameters=False,
         temperature=get_optional_arg("temperature", conf, 0.9),
-        accelerator_config="/root/.cache/huggingface/accelerate/default_config.yaml"
+        accelerator_config=conf
     )
 
