f63048ae751699ca647d06548894c60dc356aa00
Martim Afonso <martimafonso185@gmail.com>
2025-05-15 11:34:26 +0100

    Added next steps for ground truth

--- src/tactic_gen/train_decoder.py
+++ src/tactic_gen/train_decoder.py
@@ -306,7 +306,7 @@ def get_trainer(
                     temp_file.write(prefix)
 
                 reward_cache = {}
-                for completion in completions[0:3]:
+                for completion in completions:
                     if completion.strip() in reward_cache:
                         rewards.append(reward_cache[completion.strip()])
                         continue
@@ -324,7 +324,7 @@ def get_trainer(
             return rewards
         except Exception as e:
             print("error", e, temp_file_name, file=sys.stderr)
-            return [0] * len(completions[0:3])
+            return [0] * len(completions)
         finally:
             if temp_file_name:
                 os.remove(temp_file_name)
