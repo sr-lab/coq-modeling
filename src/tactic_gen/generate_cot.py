561bc9c1b7886f0cdd0dc0be83c642325287a507
Nuno Saavedra <nuno.saavedra@tecnico.ulisboa.pt>
2025-05-22 18:27:09 +0000

    openai migrate changes

--- src/tactic_gen/generate_cot.py
+++ src/tactic_gen/generate_cot.py
@@ -3,7 +3,9 @@ from tactic_gen.train_decoder import (get_datasets, init_valid_files)
 from util.train_utils import load_config
 
 
-import openai
+from openai import OpenAI
+
+client = OpenAI()
 
 model = "gpt-4.1-mini"
 
@@ -20,18 +22,16 @@ def generate_prompt(original_prompt: str, first_next_step: str) -> str:
     
     Your task is to generate a chain of thought for the next tactic.
     """
-    
+
     return system_prompt, f"{original_prompt}{first_next_step}"
 
 def call_openai(system_prompt: str, prompt: str) -> str:
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": prompt}],
-        temperature=0.7,
-        max_tokens=2000,
-    )
+    response = client.chat.completions.create(model=model,
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": prompt}],
+    temperature=0.7,
+    max_tokens=2000)
     return response.choices[0].message.content
 
 
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     parser.add_argument("--config", type=str, required=True)
     args = parser.parse_args()
 
-    
+
     config = load_config(args.config)
     if "repos_path" in config:
         init_valid_files(config["repos_path"])
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     for proof in train_dataset:
         original_prompt = proof["prompt"]
         first_next_step = proof["next_steps"][0]
-        
+
         system_prompt, prompt = generate_prompt(original_prompt, first_next_step)
         cot = call_openai(system_prompt, prompt)
         print(prompt)
