ae19d7f378477fe02265762cf3cbd12446273af2
Martim Afonso <martimafonso185@gmail.com>
2025-06-02 18:33:08 +0100

    debug

--- src/tactic_gen/train_decoder.py
+++ src/tactic_gen/train_decoder.py
@@ -127,6 +127,9 @@ def get_model(model_name: str, conf: dict[str, Any]) -> PreTrainedModel:
             load_in_8bit = False,
             full_finetuning = False,
         )
+        # Set generation config to match the defaults we want
+        model.generation_config.max_length = 4096
+        
 
     model.resize_token_embeddings(len(tokenizer))
     # https://huggingface.co/docs/bitsandbytes/main/en/fsdp_qlora
