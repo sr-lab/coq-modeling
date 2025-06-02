d41d35f8047808822aac2307dc939b21149de859
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2025-06-02 20:59:03 +0100

    debug

--- src/util/train_utils.py
+++ src/util/train_utils.py
@@ -125,9 +125,6 @@ def get_training_args(
             per_device_train_batch_size=get_required_arg(
                 "per_device_train_batch_size", conf
             ),
-            gradient_accumulation_steps=get_optional_arg(
-                "gradient_accumulation_steps", conf, 2
-            ),
             num_generations=get_optional_arg("num_generations", conf, 8),
             learning_rate=get_required_arg("learning_rate", conf),
             logging_steps=get_required_arg("logging_steps", conf),
