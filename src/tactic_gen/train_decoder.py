6b951fc88939a107cbc9ea82d5485927a9358ea3
Martim Afonso <martimafonso185@gmail.com>
2025-06-03 17:14:53 +0100

    modified config for unsloth quantized

--- src/tactic_gen/train_decoder.py
+++ src/tactic_gen/train_decoder.py
@@ -126,7 +126,7 @@ def get_model(model_name: str, conf: dict[str, Any]) -> PreTrainedModel:
                 model_name=model_name,
                 max_seq_length=conf["hard_seq_len"],
                 dtype=None,
-                full_finetuning=False,
+                #full_finetuning=False,
                 # Remove all quantization parameters - model is already quantized
             )
         else:
