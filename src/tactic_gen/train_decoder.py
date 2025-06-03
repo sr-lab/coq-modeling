e92f6f9f537298954e446625ea5fbc274a76f5cb
Martim Afonso <martimafonso185@gmail.com>
2025-06-03 16:37:36 +0100

    added force_download to conf

--- src/tactic_gen/train_decoder.py
+++ src/tactic_gen/train_decoder.py
@@ -126,6 +126,7 @@ def get_model(model_name: str, conf: dict[str, Any]) -> PreTrainedModel:
             load_in_4bit = True,
             load_in_8bit = False,
             full_finetuning = False,
+            force_download = False if "force_download" not in conf else conf["force_download"]
         )
         
 
