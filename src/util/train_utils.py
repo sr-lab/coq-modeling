5e66f9c7c314e7a6371ba033f60111594ff0efbf
Martim Afonso <martimafonso185@gmail.com>
2025-06-04 11:41:14 +0100

    define max lengths of output

--- src/util/train_utils.py
+++ src/util/train_utils.py
@@ -147,6 +147,8 @@ def get_training_args(
             ddp_find_unused_parameters=False,
             temperature=get_optional_arg("temperature", conf, 0.9),
             report_to = "tensorboard",
+            max_completion_length=get_required_arg("out_tokens", conf["example_collator"]),
+            max_prompt_length=get_required_arg("hard_seq_len", conf) - get_required_arg("out_tokens", conf["example_collator"])
             #scale_rewards=False,
         )
     elif conf["train_type"] == "sft":
