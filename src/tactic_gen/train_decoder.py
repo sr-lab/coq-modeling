8cdcdad7b498ba52bf0006272576bb6b9449e941
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2025-06-10 16:31:38 +0100

    test

--- src/tactic_gen/train_decoder.py
+++ src/tactic_gen/train_decoder.py
@@ -412,7 +412,7 @@ def get_trainer(
         EOS_TOKEN = tokenizer.eos_token
         def formatting_prompts_func(examples):
             return { "text" : [
-                example.split("[TACTIC]")[0] + "[TACTIC]<|im_end|>" + example.split("[TACTIC]")[1] + EOS_TOKEN for example in examples["text"]] 
+                example.split("[TACTIC]")[0] + "[TACTIC] <|im_end|>" + example.split("[TACTIC]")[1] + EOS_TOKEN for example in examples["text"]] 
             }
         processed_train_dataset = processed_train_dataset.map(
             formatting_prompts_func, batched = True,
