579b59b26f5504b3737eb207d2dc0cecaadfd43b
Martim Afonso <martimafonso185@gmail.com>
2025-06-18 12:26:04 +0100

    new branch

--- src/tactic_gen/train_decoder.py
+++ src/tactic_gen/train_decoder.py
@@ -482,6 +482,7 @@ def calculate_reward(
     return final_reward
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(
         description="Train code llama by providing a .yaml config file. As an example, see src/tactic_gen/confs/basic_train.yaml"
     )
