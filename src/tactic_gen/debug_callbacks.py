d88a8f4c9bb33aebc9fccdff3e5c99726962da91
Martim Afonso <martimafonso185@gmail.com>
2025-06-27 16:38:49 +0100

    debug tokenization

--- src/tactic_gen/debug_callbacks.py
+++ src/tactic_gen/debug_callbacks.py
@@ -28,6 +28,14 @@ class SimpleCallback(TrainerCallback):
         self.tokenizer = tokenizer
     
     def on_step_begin(self, args, state, control, **kwargs):
+        print(self.tokenizer.special_tokens_map)
+        print(self.tokenizer.sep_token)
+        print(self.tokenizer.eos_token)
+        print(self.tokenizer.bos_token)
+        print(self.tokenizer.unk_token)
+        print(self.tokenizer.pad_token)
+        print(self.tokenizer.mask_token)
+        print(self.tokenizer.cls_token)
         print(f"on_step_begin: {self.callback_type}")
         delimiters = ["[TACTIC", "[TACTIC]", " \n[TACTIC]\n ", "[TACTIC]\n"]
         for delimiter in delimiters:
