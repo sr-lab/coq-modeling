4a2628d32b8d907b3641852fff56982e7bca188a
Martim Afonso <martimafonso185@gmail.com>
2025-07-01 09:52:55 +0100

    added chat_template

--- src/tactic_gen/unsloth_train_decoder.py
+++ src/tactic_gen/unsloth_train_decoder.py
@@ -280,7 +280,7 @@ def get_sft_trainer(
     processed_train_dataset = processed_train_dataset.map(
         formatting_prompts_func, batched = True,
     )
-    print("processed_train_dataset: ", processed_train_dataset["text"][0])
+    print("processed_train_dataset: ", processed_train_dataset["text"])
 
     EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
     def format_dataset_prompt(examples):
