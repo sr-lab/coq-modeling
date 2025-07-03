00bb1e11932c7af2d79f365a86bcbe25cb306f35
Martim Afonso <martimafonso185@gmail.com>
2025-07-03 16:59:41 +0100

    added new model wrapper for unsloth

--- src/model_deployment/model_wrapper.py
+++ src/model_deployment/model_wrapper.py
@@ -134,6 +134,138 @@ class DecoderLocalWrapper:
         self.hard_seq_len = hard_seq_len
         self.max_new_tokens = max_new_tokens
         
+    def get_recs(
+        self,
+        example: LmExample,
+        n: int,
+        current_proof: str,
+        beam: bool,
+        token_mask_str,
+    ) -> ModelResult:
+        token_mask = None
+        if token_mask_str is not None:
+            token_mask = TokenMask.from_str(token_mask_str)
+        collated_input = self.collator.collate_input(self.tokenizer, example)
+
+        inputs = self.tokenizer(
+            collated_input,
+            max_length=self.hard_seq_len,
+            truncation=True,
+            return_tensors="pt",
+        )
+
+        attention_mask = transform_attention_mask(
+            self.collator,
+            self.tokenizer,
+            token_mask,
+            inputs["input_ids"],
+            inputs["attention_mask"],
+        )
+        with torch.no_grad():
+            outputs = self.model.generate(
+                inputs["input_ids"].cuda(),
+                max_new_tokens=self.max_new_tokens,
+                return_dict_in_generate=True,
+                output_scores=True,
+                length_penalty=0,
+                num_return_sequences=n,
+                temperature=None if beam else 1,
+                do_sample=not beam,
+                num_beams=n if beam and 1 < n else 1,
+                attention_mask=attention_mask.cuda(),
+            )
+        input_num_tokens = inputs["input_ids"].shape[1]
+        generated_seqs = outputs.sequences[:, input_num_tokens:]
+        aux_tactics = self.tokenizer.batch_decode(generated_seqs, skip_special_tokens=True)
+        non_special_tokens = torch.concat(
+            [(generated_seqs != t)[:, :, None] for t in self.tokenizer.all_special_ids],
+            axis=2,
+        ).all(dim=2)
+        lengths = non_special_tokens.sum(axis=1).tolist()
+
+        tactics = []
+        for tactic in aux_tactics:
+            if ReasoningCollator.check_format(tactic):
+                tactics.append(ReasoningCollator.extract_tactic(tactic))
+            else:
+                tactics.append(tactic)
+
+        if beam and 1 < n:
+            scores = outputs.sequences_scores.tolist()
+            return ModelResult(tactics, scores, lengths)
+        else:
+            with torch.no_grad():
+                transition_scores = self.model.compute_transition_scores(
+                    generated_seqs, outputs.scores, normalize_logits=True
+                )
+                scores = (
+                    transition_scores.where(
+                        transition_scores != -torch.inf, torch.tensor(0.0)
+                    )
+                    .sum(axis=1)
+                    .tolist()
+                )
+                return ModelResult(tactics, scores, lengths)
+
+    @classmethod
+    def get_training_conf(cls, checkpoint_loc: Path) -> Any:
+        training_conf_loc = checkpoint_loc.parent / "training_conf.yaml"
+        with training_conf_loc.open("r") as f:
+            training_conf = yaml.safe_load(f)
+        return training_conf
+
+    @classmethod
+    def from_checkpoint(cls, checkpoint_loc: Path, conf: dict[str, Any]) -> DecoderLocalWrapper:
+        training_conf = cls.get_training_conf(checkpoint_loc)
+        hard_seq_length = get_required_arg("hard_seq_len", training_conf)
+        example_collator_conf = example_collator_conf_from_yaml(
+            training_conf["example_collator"]
+        )
+        example_collator = example_collator_from_conf(example_collator_conf)
+
+        model, tokenizer = process_model(str(checkpoint_loc.resolve()), training_conf)
+        model.to("cuda")
+        
+        if tokenizer is None:
+            tokenizer = get_tokenizer(
+                get_required_arg("model_name", training_conf), add_eos=False
+            )
+
+        if "max_new_tokens" in conf:
+            max_new_tokens = conf["max_new_tokens"]
+        else:
+            max_new_tokens = 128
+        return cls(
+            model, 
+            tokenizer, 
+            example_collator, 
+            hard_seq_length, 
+            max_new_tokens
+        )
+
+    @classmethod
+    def from_conf(cls, json_data: Any) -> DecoderLocalWrapper:
+        name = json_data["checkpoint_loc"]
+        return cls.from_checkpoint(Path(name), json_data)
+
+
+class UnslothDecoderLocalWrapper:
+    ALIAS = "unsloth-decoder-local"
+
+    def __init__(
+        self,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+        collator: ExampleCollator,
+        hard_seq_len: int,
+        max_new_tokens: int = 128,
+    ):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.collator = collator
+        self.hard_seq_len = hard_seq_len
+        self.max_new_tokens = max_new_tokens
+        
     def apply_chat_template(self, collated_input: str) -> str:
         if "\n[TACTIC]\n" in collated_input:
             user_part, assistant_part = collated_input.split("\n[TACTIC]\n", 1)
@@ -165,25 +297,6 @@ generate only the next tactic."},
             token_mask = TokenMask.from_str(token_mask_str)
         collated_input = self.collator.collate_input(self.tokenizer, example)
         collated_input = self.apply_chat_template(collated_input)
-
-        from vllm import SamplingParams
-        sampling_params = SamplingParams(
-            temperature=0.8,
-            max_tokens=self.max_new_tokens,
-            top_p=0.95,
-            top_k=1,
-        )
-
-        # output = self.model.fast_generate(
-        #     [collated_input],
-        #     sampling_params = sampling_params,
-        #     lora_request = None,
-        # )[0].outputs[0].text
-
-        # print("OUTPUT:")
-        # print(output)
-        # print("-" * 50)
-
 
         inputs = self.tokenizer(
             collated_input,
@@ -316,6 +429,8 @@ def wrapper_from_conf(conf: Any) -> ModelWrapper:
     match attempted_alias:
         case DecoderLocalWrapper.ALIAS:
             return DecoderLocalWrapper.from_conf(conf)
+        case UnslothDecoderLocalWrapper.ALIAS:
+            return UnslothDecoderLocalWrapper.from_conf(conf)
         case _:
             raise WrapperNotFoundError(
                 f"Could not find model wrapper: {attempted_alias}"
